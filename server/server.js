611f469a07a27b43f642c7f31dcb9ec4a2e3b787
Stephen Helman <stephenhelman18@gmail.com>
2025-02-11 10:54:15 -0700

    yay

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use("/api", require("./routes/pdfRouter.js"));
 
 if (process.env.NODE_ENV === "production") {
-  console.log(__dirname);
+  app.use(express.static(path.join(__dirname, "../client/dist")));
   app.use("*", (req, res) => {
     res.sendFile(path.join(__dirname, "..", "client", "dist", "index.html"));
   });
